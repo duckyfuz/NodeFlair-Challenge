7fc4ef2580578b51714d03904f6572e8a4121dd9
duckyfuz <kenf@iMac&cheese.local>
2023-08-19 10:36:37 +0800

    Add loading skeleton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 
-import { ChakraProvider, Box, Text, theme, HStack } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  theme,
+  HStack,
+  Skeleton,
+  Card,
+} from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 import data from "./test/data.json";
@@ -12,6 +20,7 @@ export const App = () => {
     window.innerHeight,
   ]);
   const [selectedJob, setSelectedJob] = useState("1");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const handleWindowResize = () => {
@@ -23,6 +32,14 @@ export const App = () => {
     return () => {
       window.removeEventListener("resize", handleWindowResize);
     };
+  }, []);
+
+  // Mock time taken to fetch data
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 800);
   }, []);
 
   return (
@@ -40,29 +57,45 @@ export const App = () => {
               job={job}
               selectedJob={selectedJob}
               setSelectedJob={setSelectedJob}
+              isLoading={isLoading}
             />
           ))}
         </Box>
-        <Box w={675}></Box>
-        <Box
+        <Card w={675} />
+        <Skeleton
+          isLoaded={!isLoading}
           position={"fixed"}
           top={0}
           right={(window.innerWidth - 425 - 675 - 40) / 2}
+          borderRadius={"lg"}
           h={window.innerHeight}
           // Add dynamic sizing if there's time
           // maxW={675}
           // minW={650}
           w={675}
-          backgroundColor={"white"}
-          boxShadow="base"
-          borderColor={theme.colors.gray["300"]}
-          borderWidth={1}
-          p={4}
-        >
-          <Text align={"start"} as="b" fontSize="4xl">
-            {data.find((job) => job.ID === selectedJob)["Job Title"]}
-          </Text>
-        </Box>
+        />
+        {!isLoading && (
+          <Card
+            position={"fixed"}
+            top={0}
+            right={(window.innerWidth - 425 - 675 - 40) / 2}
+            h={window.innerHeight}
+            // Add dynamic sizing if there's time
+            // maxW={675}
+            // minW={650}
+            w={675}
+            backgroundColor={"white"}
+            boxShadow="base"
+            borderRadius={"lg"}
+            borderColor={theme.colors.gray["300"]}
+            borderWidth={1}
+            p={4}
+          >
+            <Text align={"start"} as="b" fontSize="4xl">
+              {data.find((job) => job.ID === selectedJob)["Job Title"]}
+            </Text>
+          </Card>
+        )}
       </HStack>
     </ChakraProvider>
   );
--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -13,6 +13,9 @@ import {
   Image,
   theme,
   Icon,
+  Skeleton,
+  SkeletonText,
+  SkeletonCircle,
 } from "@chakra-ui/react";
 
 import { StarIcon } from "@chakra-ui/icons";
@@ -45,7 +48,7 @@ const TechCard = ({ skill }: any) => {
   );
 };
 
-const JobCard = ({ job, selectedJob, setSelectedJob }: any) => {
+const JobCard = ({ job, selectedJob, setSelectedJob, isLoading }: any) => {
   const [displayNo, setDisplayNo] = useState([0, false]);
 
   useEffect(() => {
@@ -103,75 +106,78 @@ const JobCard = ({ job, selectedJob, setSelectedJob }: any) => {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <Card
-        w={425}
-        px={5}
-        key={job["Job Title"]}
-        borderRadius={"lg"}
-        borderColor={
-          selectedJob === job.ID
-            ? theme.colors.green["400"]
-            : theme.colors.gray["300"]
-        }
-        borderWidth={selectedJob === job.ID ? 3 : 0.5}
-        onClick={jobClickHandler}
-      >
-        <CardBody p={0}>
-          <HStack justifyContent={"space-between"} align={"start"} my={5}>
-            <HStack align={"start"}>
-              <Image src={job["Logo"]} boxSize="50px" borderRadius={5} />
-              <VStack align={"start"} gap={0}>
-                <HStack gap={0}>
-                  <Text fontSize="sm" mr={3} noOfLines={1}>
-                    {job["Company Name"]}
+      <Skeleton isLoaded={!isLoading} borderRadius={"lg"}>
+        <Card
+          w={425}
+          minH={150}
+          px={5}
+          key={job["Job Title"]}
+          borderRadius={"lg"}
+          borderColor={
+            selectedJob === job.ID
+              ? theme.colors.green["400"]
+              : theme.colors.gray["300"]
+          }
+          borderWidth={selectedJob === job.ID ? 3 : 0.5}
+          onClick={jobClickHandler}
+        >
+          <CardBody p={0}>
+            <HStack justifyContent={"space-between"} align={"start"} my={5}>
+              <HStack align={"start"}>
+                <Image src={job["Logo"]} boxSize="50px" borderRadius={5} />
+                <VStack align={"start"} gap={0}>
+                  <HStack gap={0}>
+                    <Text fontSize="sm" mr={3} noOfLines={1}>
+                      {job["Company Name"]}
+                    </Text>
+                    {job["Rating"] && (
+                      <>
+                        <Text fontSize="sm" mr={1}>
+                          {job["Rating"]}
+                        </Text>
+                        <StarIcon />
+                      </>
+                    )}
+                  </HStack>
+                  <Text as="b" noOfLines={2}>
+                    {job["Job Title"]}
                   </Text>
-                  {job["Rating"] && (
-                    <>
-                      <Text fontSize="sm" mr={1}>
-                        {job["Rating"]}
-                      </Text>
-                      <StarIcon />
-                    </>
-                  )}
-                </HStack>
-                <Text as="b" noOfLines={2}>
-                  {job["Job Title"]}
-                </Text>
-                <HStack justify={"start"} gap={0}>
-                  <Text
-                    mr={3}
-                    as="b"
-                    fontSize="sm"
-                    color={theme.colors.green[500]}
-                  >
-                    {formatTimeAgo(job["Created Date"])}
+                  <HStack justify={"start"} gap={0}>
+                    <Text
+                      mr={3}
+                      as="b"
+                      fontSize="sm"
+                      color={theme.colors.green[500]}
+                    >
+                      {formatTimeAgo(job["Created Date"])}
+                    </Text>
+                    <Icon as={MdLocationOn} />
+                    <Text noOfLines={1} minW={5} fontSize="sm">
+                      {job["Location"]}
+                    </Text>
+                  </HStack>
+                  <Text as="b" fontSize="sm">
+                    {job["Pay Range"]}
                   </Text>
-                  <Icon as={MdLocationOn} />
-                  <Text noOfLines={1} minW={5} fontSize="sm">
-                    {job["Location"]}
-                  </Text>
-                </HStack>
-                <Text as="b" fontSize="sm">
-                  {job["Pay Range"]}
-                </Text>
-              </VStack>
+                </VStack>
+              </HStack>
+              <Stack>
+                <TypeCard type={job["Type"]} />
+              </Stack>
             </HStack>
-            <Stack>
-              <TypeCard type={job["Type"]} />
-            </Stack>
-          </HStack>
-        </CardBody>
-        <Divider />
-        <CardFooter m={0} py={2} px={0}>
-          <HStack gap={1} minH={8}>
-            {job["Tech Stacks"] &&
-              job["Tech Stacks"]
-                .slice(0, displayNo[0])
-                .map((tech: any) => <TechCard key={tech} skill={tech} />)}
-            <Icon as={MdMoreHoriz} />
-          </HStack>
-        </CardFooter>
-      </Card>
+          </CardBody>
+          <Divider />
+          <CardFooter m={0} py={2} px={0}>
+            <HStack gap={1} minH={8}>
+              {job["Tech Stacks"] &&
+                job["Tech Stacks"]
+                  .slice(0, displayNo[0])
+                  .map((tech: any) => <TechCard key={tech} skill={tech} />)}
+              {displayNo[1] && <Icon as={MdMoreHoriz} />}
+            </HStack>
+          </CardFooter>
+        </Card>
+      </Skeleton>
     </animated.div>
   );
 };
